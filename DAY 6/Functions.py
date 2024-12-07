161524226b02d5d43e9fc902d2ddf1e5ae238751
Nyasha Nallen Tumba <nyashatumba@gmail.com>
2024-12-07 10:54:12 +0200

    Functions
    Lambda Functions addition

--- DAY 6/Functions.py
+++ DAY 6/Functions.py
@@ -11,4 +11,10 @@ def getSum(num1, num2):
     return total
 num = getSum(3, 4)
 print(num)  #To output the returned value to the console
+
+#LAMBPA FUNCTIONS
+# Its a small anonymous fn tt takes any number of arguments but can only have one expression
+
+getSum = lambda num1, num2 : num1 + num2
+print(getSum(10, 3))
     
