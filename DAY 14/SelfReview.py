82bd644eb0652ba37752419787becb6793d9f523
Nyasha Nallen Tumba <nyashatumba@gmail.com>
2025-02-24 15:38:56 +0200

    Day 14 - OOP

---
+++ DAY 14/SelfReview.py
@@ -0,0 +1,47 @@
+'''
+QN 1. Were you able to complete the code and get the expected final output mentioned?
+Yes OR No
+
+Question 2
+What was the part that you were not able to complete? Specify the line numbers in the 8 lines of code.
+
+The expected code for the program is as follows:
+class MyFirstClass():
+    print("Who wrote this?")
+    index = "Author-Book"
+    def hand_list(self, philosopher, book):
+        print(MyFirstClass.index)
+        print(philosopher + " wrote the book: " + book)
+whodunnit = MyFirstClass()
+whodunnit.hand_list("Sun Tzu", "The Art of War")
+
+2
+None
+4
+5
+8
+6
+7
+1
+3
+
+Question 3
+Which of the following is the class variable in the code above?
+
+MyFirstClass
+index
+philosopher
+whodunnit
+4.
+Question 4
+How will you modify the code below if you want to include a “year” of publication in the output?
+class MyFirstClass():
+    print("Who wrote this?")
+    index = "Author-Book"
+    def hand_list(self, philosopher, book):
+        print(MyFirstClass.index)
+        print(philosopher + " wrote the book: " + book)
+whodunnit = MyFirstClass()
+whodunnit.hand_list("Sun Tzu", "The Art of War")
+Insert your suggestions below 
+'''
